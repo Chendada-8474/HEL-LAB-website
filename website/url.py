a796df32ed74607bfd73f31101dbee9eb91fdd33
ChenDada <fws200803@gmail.com>
2021-06-21 12:20:38 +0800

    ready for demo

--- website/url.py
+++ website/url.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('project.html', views.project, name="project"),
 ]
--- website/views.py
+++ website/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "home.html", {})
+
+def project(request):
+    return render(request, "project.html", {})
